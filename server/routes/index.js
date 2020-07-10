67fd0dfd24931cc903edc41ff07123ab50351ccd
Deborah Espíndola <deo2@cin.ufpe.br>
2020-07-10 14:39:46 -0300

    editing files before understanding more about keystone

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,20 @@
-// const path = require('path');
-// const keystone = require('keystone');
-// const cors = require('cors');
+const path = require('path');
+const keystone = require('keystone');
+const cors = require('cors');
 
-// const Example = keystone.list('Examples');
+const Menu = keystone.list('menu-elements');
 
-// module.exports = (app) => {
-//   app.use(cors());
+module.exports = (app) => {
+  app.use(cors());
 
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/index.html'));
-//   });
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
 
-//   app.get('/api/examples', (req, res) => {
-//     Example.model.find((err, items) => {
-//       if (err) return res.apiError('database error', err);
-//       res.send(items);
-//     });
-//   });
-// };
+  app.get('/api/menu-elements', (req, res) => {
+    Menu.model.find((err, items) => {
+      if (err) return res.apiError('database error', err);
+      res.send(items);
+    });
+  });
+};
